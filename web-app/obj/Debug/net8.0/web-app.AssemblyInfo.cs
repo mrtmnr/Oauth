e7b2c05a654bf1f6680aa38d5aadad639e2acd87
mrtmnr <mertmunar@gmail.com>
2024-07-27 19:45:20 +0300

    second commit

--- web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
+++ web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36ba529c33062fffcc0981ff45d484cfd195fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
